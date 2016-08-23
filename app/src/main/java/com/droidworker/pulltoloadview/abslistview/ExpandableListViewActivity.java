84e70381c660d9ef482ca79d479ae449349f07eb
luoyanfeng <lyf91112@126.com>
2016-08-23 17:53:43 +0800

    准备创建新的扩展

---
+++ app/src/main/java/com/droidworker/pulltoloadview/abslistview/ExpandableListViewActivity.java
@@ -0,0 +1,24 @@
+package com.droidworker.pulltoloadview.abslistview;
+
+import com.droidworker.lib.PullToLoadBaseView;
+import com.droidworker.pulltoloadview.BaseActivity;
+
+/**
+ * @author https://github.com/DroidWorkerLYF
+ */
+public class ExpandableListViewActivity extends BaseActivity {
+    @Override
+    public void onLoadNew() {
+
+    }
+
+    @Override
+    public void onLoadMore() {
+
+    }
+
+    @Override
+    protected PullToLoadBaseView getPullToLoadView() {
+        return null;
+    }
+}
---
+++ extend/src/main/java/com/droidworker/extend/abslistview/PullToLoadExpandableListView.java
@@ -0,0 +1,7 @@
+package com.droidworker.extend.abslistview;
+
+/**
+ * @author https://github.com/DroidWorkerLYF
+ */
+public class PullToLoadExpandableListView {
+}
